d14030a3ad33eff44276a88c50f62c136e27ff0a
Utsav Barnwal <123968318+Fiesta11@users.noreply.github.com>
2024-01-11 10:09:08 +0000

    OverView

---
+++ assignment/app.js
@@ -0,0 +1,18 @@
+const { log } = require('console')
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+app.use(express.json())
+mongoose.connect('mongodb+srv://utsav123:mongodb@cluster0.yctsegm.mongodb.net/')
+.then(()=> console.log("Connection Done"))
+.catch(err => console.error("Could not connect to mongodb" , err))
+
+const port = process.env.port || 3000;
+app.get("/" , (req , res) =>{
+    res.send("Hello Github Codespaces")
+})
+
+app.listen(port , ()=>{
+    console.log(`Server is Running on ${port}`);
+})
