d2df8cfb6745672d83b18d015c46a11710802c98
Utsav Barnwal <123968318+Fiesta11@users.noreply.github.com>
2024-01-12 14:46:15 +0530

    Delete practice_Code directory

--- practice_Code/models/post.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const post_schema = new mongoose.Schema({
-    id : {type : String , required : true , unique : true} ,
-    text : {type :String , required : true}
-})
-
-module.exports = mongoose.model('Post' , post_schema)
--- practice_Code/src/controllers/post_control.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const post_service = require('../services/post_service')
-
-router.post('/api/v1/posts' , post_service.create_post)
-// router.get('/api/v1/posts/:id/analysis/' , post_service.get_post_analysis);
-
-module.exports = router;
